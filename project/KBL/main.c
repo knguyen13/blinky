47e3a61ed14c81838e588bb896dd9d2a22483a8c
knguyen <knguyen9328@gmail.com>
2022-01-08 19:26:41 -0800

    set up microchip environment

---
+++ project/KBL/main.c
@@ -0,0 +1,18 @@
+/*
+ * KBL.c
+ *
+ * Created: 1/8/2022 7:23:05 PM
+ * Author : knguyen
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

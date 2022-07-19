dde12ad13b4fc8726f4336524349c2de5664dc6c
knguyen <knguyen9328@gmail.com>
2022-07-19 15:21:57 -0700

    remove test code

--- project/blinky/blinky/main.c
+++ project/blinky/blinky/main.c
@@ -10,10 +10,6 @@
 
 int main(void)
 {
-	gpio_led_en();
-	gpio_led_t settings = {GPIO_LED_ALL, GPIO_LED_BLUE};
-	gpio_led_config(settings);
-    /* Replace with your application code */
     while (1) 
     {
     }
